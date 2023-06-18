73cb5064504ff4e4955c886134e9dbdcaee901d9
selenakinn <akinselen15@gmail.com>
2023-06-19 00:03:15 +0300

    v1.13

---
+++ internet based programming proje/script.js
@@ -0,0 +1,18 @@
+const arrows = document.querySelectorAll(".arrow");
+const movieLists = document.querySelectorAll(".movie-list");
+
+arrows.forEach((arrow, i) => {
+  let clickCounter = 0;
+  const itemNumber = movieLists[i].querySelectorAll("img").length;
+  console.log(itemNumber);
+  arrow.addEventListener("click", function () {
+
+    clickCounter++;
+    if (itemNumber - (4 + clickCounter) >= 0) {
+      movieLists[i].style.transform = `translateX(${movieLists[i].computedStyleMap().get("transform")[0].x.value - 300}px)`;
+
+    } else {
+      movieLists[i].style.transform = "translateX(0)";
+    }
+  });
+});
